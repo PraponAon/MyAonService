0f0022b53e835e3e31cc6b0c32272be423485960
prapon <prapon@live.com>
2017-11-19 14:51:21 +0700

    Show Date Success

--- app/src/main/java/com/vdevsoft/prapon/myaonservice/fragment/SecondFragment.java
+++ app/src/main/java/com/vdevsoft/prapon/myaonservice/fragment/SecondFragment.java
@@ -7,7 +7,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vdevsoft.prapon.myaonservice.R;
 import com.vdevsoft.prapon.myaonservice.utility.GetJSON;
@@ -34,13 +37,61 @@ public class SecondFragment extends android.support.v4.app.Fragment{
 //        read Alll JSON to Class
         readJSON();
 
-//      showrate
-
 //      Show date, drag click ขวา extract สน้าง method
         showDate();
 
+//      showrate
+        showRate();
 
+//        calculate controller
+        calculateController();
     } // main Method
+
+    private void calculateController() {
+
+        Button button = getView().findViewById(R.id.btnExchange);
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               EditText editText = getView().findViewById(R.id.edtUSD);
+               usdString = editText.getText().toString().trim();
+
+               if (usdString.isEmpty()) {
+                   Toast.makeText(getActivity(), "Please Fill Thai Bath",
+                           Toast.LENGTH_SHORT).show();  // 5 วินาที  , long 9 วินาที
+               } else {
+//                   no space
+                   double usdAdouble = Double.parseDouble(usdString);
+                   double answerAdouble = usdAdouble * rateADouble;
+
+                   TextView textView = getView().findViewById(R.id.txtAnswer);
+                   textView.setText(Double.toString(answerAdouble)+"THB");
+
+               }
+           }
+       });
+
+
+    }
+
+    private void showRate() {
+        TextView textView = getView().findViewById(R.id.txtShowRate);
+
+        try {
+            JSONArray jsonArray = new JSONArray(jsonRateString);
+            JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+            rateADouble = jsonObject.getDouble("THB");
+            Log.d("19novV1", "rateADouble ===> " + rateADouble);
+
+            textView.setText(getString(R.string.rate) + Double.toString(rateADouble));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
     private void showDate() {
 //        shit+ctrl+enter   สร้าง กรอบ { } completment
@@ -58,56 +109,6 @@ public class SecondFragment extends android.support.v4.app.Fragment{
 
             jsonRateString = "[" + jsonObject.getString("rates") + "]";
             Log.d("19novV1","jsonRate ==> " + jsonRateString);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         } catch (Exception e) {
             e.printStackTrace();
